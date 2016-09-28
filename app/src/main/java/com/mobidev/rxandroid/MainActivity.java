ba8968ff9c1c3dec24bcb1c65f3c89cbd7c2be51
Lawrence Macharia <macharialawrence2011@gmail.com>
2016-09-28 07:34:56 +0300

    initial commit

---
+++ app/src/main/java/com/mobidev/rxandroid/MainActivity.java
@@ -0,0 +1,56 @@
+package com.mobidev.rxandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import rx.Observable;
+import rx.Observer;
+import rx.Subscription;
+import rx.functions.Func1;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Subscription subscription;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Observable<Integer> observable = Observable
+                .just(1, 2, 3, 4, 5)
+                .map(new Func1<Integer, Integer>() {
+                    @Override
+                    public Integer call(Integer integer) {
+                        return (integer % 2 != 0) ? integer * 2 : integer;
+                    }
+                });
+
+        subscription = observable.subscribe(new Observer<Integer>() {
+            @Override
+            public void onCompleted() {
+                Log.d("Test", "In onCompleted()");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d("Test", "In onError()");
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Log.d("Test", "In onNext():" + integer);
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (subscription != null) {
+            subscription.unsubscribe();
+        }
+    }
+}
